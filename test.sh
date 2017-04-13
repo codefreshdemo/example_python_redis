f0a177d0b4d3722d28487fa2cc2db28d2fe3b019
nikolai <n.ivanov.p@gmail.com>
2017-04-13 15:09:50 +0300

    added wait-for-db method

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
-sleep 5
+#!bin/bash
+
+wait_for_db() {
+  nslookup redis
+  if ! nc -z redis 6379; then
+    echo "Waiting for db..."
+    sleep 2
+    wait_for_db
+  fi
+}
+
+wait_for_db
+
 if curl web | grep -q '<b>Visits:</b> '; then
   echo "Tests passed!"
   exit 0
