1c98e6ff13857e83dbfef830ad9248c8ba42fe60
nikolai <n.ivanov.p@gmail.com>
2017-04-13 15:15:32 +0300

    updated test script

--- test.sh
+++ test.sh
@@ -9,7 +9,17 @@ wait_for_db() {
   fi
 }
 
+wait_for_web() {
+  nslookup web
+  if ! nc -z web 80; then
+    echo "Waiting for web..."
+    sleep 2
+    wait_for_web
+  fi
+}
+
 wait_for_db
+wait_for_web
 
 if curl web | grep -q '<b>Visits:</b> '; then
   echo "Tests passed!"
