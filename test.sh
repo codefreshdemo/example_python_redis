cfa633d210f85a4e7f9d4890753dc4b377ef34ec
Kostis Kapelonis <kostis@codefresh.io>
2019-09-06 22:28:58 +0300

    Using service containers instead of compositions

--- test.sh
+++ test.sh
@@ -1,26 +1,5 @@
 #!bin/bash
 
-wait_for_db() {
-  nslookup redis
-  if ! nc -z redis 6379; then
-    echo "Waiting for db..."
-    sleep 2
-    wait_for_db
-  fi
-}
-
-wait_for_web() {
-  nslookup web
-  if ! nc -z web 80; then
-    echo "Waiting for web..."
-    sleep 2
-    wait_for_web
-  fi
-}
-
-wait_for_db
-wait_for_web
-
 if curl web | grep -q '<b>Visits:</b> '; then
   echo "Tests passed!"
   exit 0
